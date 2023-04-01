94ac3d9d78bb34813226402c8f1f62727fcc5fd1
MatthewDAgron <mdagron907@ou.edu>
2023-04-01 14:07:11 -0500

    initial webpage and bluetooth, package.json

--- Hacklahoma/src/bluetoothDetection.js
+++ Hacklahoma/src/bluetoothDetection.js
@@ -1,1 +1,19 @@
-alert('alee is a boss')
+function onClickStart(){
+    checkBlueTooth()
+    alert('not done yet')
+}
+
+function checkBlueTooth(){
+    if ('bluetooth' in navigator) {
+        navigator.bluetooth.requestDevice({ acceptAllDevices: true })
+          .then(device => {
+            console.log(`Bluetooth device detected: ${device.name}`);
+            // Do something with the device
+          })
+          .catch(error => {
+            console.log(`Error detecting Bluetooth device: ${error}`);
+          });
+      } else {
+        console.log('Web Bluetooth API is not supported in this browser.');
+      }
+}
