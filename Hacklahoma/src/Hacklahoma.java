b6d51fda7c1d4c14a2ca4e357c09cc3d9c4d16bb
Raleigh-Savage <raleigh.d.savage@ou.edu>
2023-04-01 13:55:45 -0500

    switch statement added

--- Hacklahoma/src/Hacklahoma.java
+++ Hacklahoma/src/Hacklahoma.java
@@ -1,4 +1,68 @@
+import java.util.ArrayList;
 
-public class Hacklahoma {
-
+public class Hacklahoma 
+{
+	public Character input = 'a';
+	
+	public void translator()
+	{
+		switch(input)
+		{
+		case 'a':
+			 //do something
+		case 'b':
+			 //do something
+		case 'c':
+			 //do something
+		case 'd':
+			 //do something
+		case 'e':
+			 //do something
+		case 'f':
+			 //do something
+		case 'g':
+			 //do something
+		case 'h':
+			 //do something
+		case 'i':
+			 //do something
+		case 'j':
+			 //do something
+		case 'k':
+			 //do something
+		case 'l':
+			 //do something
+		case 'm':
+			 //do something
+		case 'n':
+			 //do something
+		case 'o':
+			 //do something
+		case 'p':
+			 //do something
+		case 'q':
+			 //do something
+		case 'r':
+			 //do something
+		case 's':
+			 //do something
+		case 't':
+			 //do something
+		case 'u':
+			 //do something
+		case 'v':
+			 //do something
+		case 'w':
+			 //do something
+		case 'x':
+			 //do something
+		case 'y':
+			 //do something
+		case 'z':
+			 //do something
+		
+		}
+	}
 }
+
+
