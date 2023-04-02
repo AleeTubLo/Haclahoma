66787c69b272f2c7304beda5f7c5de582ee7b1c1
Tub Alee Lo <tub.a.lo@ou.edu>
2023-04-02 01:49:51 -0500

    Created the new Character to Binary, use this one, and updated the arduino code.

---
+++ website/src/Downloadable/Character to Binary/Character to Binary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO.Ports;
+namespace CharacterToBinary
+{
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            SerialPort port = new SerialPort("COM5", 9600);
+
+            port.Open();
+
+            Console.Write("Enter your sequence of letters you with to convert to braille:");
+
+            string input = Console.ReadLine();
+            
+            byte[] bytesOfInput = System.Text.Encoding.ASCII.GetBytes(input);
+
+            foreach (byte b in bytesOfInput)
+            {
+
+                string binary = Convert.ToString(b, 2).PadLeft(8, '0');
+                port.Write(binary);
+
+            }
+
+            port.Close();
+
+        }
+
+
+    }
+
+
+}
